54a68a1f0bda12adbfc274997bdcb8c0312819cd
Michael Liang <liangmicha@gmail.com>
2015-04-25 17:35:55 -0400

    oman

---
+++ app.py
@@ -0,0 +1,28 @@
+import algorithms
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', data=None)
+
+
+@app.route('/jsonreq', methods=['POST'])
+def jsonreq(request):
+    # Get the JSON data sent from the form
+    jsondata = request.form['searchquery']
+    print jsondata
+    # Convert the JSON data into a Python structure
+    return render_template('results.html')
+
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    email = request.form['search-query']
+    results = "some json object %s" %(email) 
+    return render_template("results.html", results=results)
+
+if __name__ == '__main__':
+    app.run()
